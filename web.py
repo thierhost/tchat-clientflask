61160ad4ea84980ed0765fb743f8537a96fb30a8
thierno thiam <thiernothiam@esp.sn>
2017-02-25 00:37:37 +0000

    commande running in Procfile

--- web.py
+++ web.py
@@ -18,6 +18,6 @@ def discussions():
 
 if __name__ == '__main__':
 	app.debug = True
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 8080))
 	app.run(host='0.0.0.0', port=port)
 	
